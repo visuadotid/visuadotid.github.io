68e9d06ebdd714b592106abcb01eadfb7fa2f6c6
Nur Shalahuddin Fajri <shotozuro@gmail.com>
2020-11-04 01:23:22 +0700

    install gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
+import React, { useEffect } from 'react';
+import { GTMPageView } from '../lib/gtm';
 import '../src/css/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  // Initiate GTM
+  useEffect(() => {
+    const handleRouteChange = (url) => GTMPageView(url);
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
