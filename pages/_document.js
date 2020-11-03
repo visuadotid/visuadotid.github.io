0c283bd89d13af78a9a377f1375dd178df0dd4ea
Nur Shalahuddin Fajri <shotozuro@gmail.com>
2020-11-04 06:22:24 +0700

    remove unused code

--- pages/_document.js
+++ pages/_document.js
@@ -7,26 +7,24 @@ export default class MyDocument extends Document {
     return (
       <Html lang="id">
         <Head>
-          <>
-            {/* Google Tag Manager */}
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','${GTM_ID}');`,
-              }}
-            />
-            {/* End Google Tag Manager */}
-            {/* Google Tag Manager (noscript) */}
-            <noscript
-              dangerouslySetInnerHTML={{
-                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-              }}
-            />
-            {/* End Google Tag Manager (noscript) */}
-          </>
+            }}
+          />
+          {/* End Google Tag Manager */}
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
         </Head>
         <body>
           <Main />
