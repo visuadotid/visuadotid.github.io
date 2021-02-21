a9edaedf9e7aba1e621a46f412312759ca81b0de
Nur Shalahuddin Fajri <shotozuro@gmail.com>
2021-02-21 12:55:18 +0700

    Deploy visua.id to gh-pages

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c0e1fda0eda4635ddfce";
+/******/ 	var hotCurrentHash = "d8cf36fee805ee88245a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
